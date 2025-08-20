46e6a91a9c9143e649d676ff773d024c97ce5e79
rzkshrl <rizkysahrul0@gmail.com>
2025-08-20 10:19:05 +0700

    web.php route

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/upload-file', [FileUpload::class, 'createForm']);
-Route::post('/upload-file', [FileUpload::class, 'fileUpload'])->name('fileUpload');
+// Route::get('/upload-file', [FileUpload::class, 'createForm']);
+// Route::post('/upload-file', [FileUpload::class, 'fileUpload'])->name('fileUpload');
 
 Route::get('/upload', [FileUpload::class, 'index'])->name('file.index');
 Route::post('/upload', [FileUpload::class, 'store'])->name('file.upload');
