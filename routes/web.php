5ff4f60d75a867b81423e4ee1273323d564880cd
rzkshrl <rizkysahrul0@gmail.com>
2025-08-20 10:25:50 +0700

    delete route def

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/upload-file', [FileUpload::class, 'createForm']);
-// Route::post('/upload-file', [FileUpload::class, 'fileUpload'])->name('fileUpload');
-
 Route::get('/upload', [FileUpload::class, 'index'])->name('file.index');
 Route::post('/upload', [FileUpload::class, 'store'])->name('file.upload');
 Route::get('/file-download/{id}', [FileUpload::class, 'download'])->name('file.download');
