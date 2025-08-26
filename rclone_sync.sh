e8f04768b5d773fbb0a83a49fe84de709cfa01dc
rzkshrl <rizkysahrul0@gmail.com>
2025-08-26 14:14:45 +0700

    rclone_sync.sh

--- rclone_sync.sh
+++ rclone_sync.sh
@@ -3,6 +3,8 @@ DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 echo "[$DATE] Start sync..." >> /volume1/scripts/rclone.log
 
+rclone sync "gdrive:/Form Upload Dokumen Desa (File responses)/Respon Spreadsheet" "/volume1/homes/adminpemdes/FormResponseGDriveSync" --drive-export-formats xlsx,docx,pdf --log-file="/volume1/scripts/rclone.log" -v
+
 rclone sync "gdrive:/Form Upload Dokumen Desa (File responses)/Upload Dokumen (File responses)" "/volume1/homes/adminpemdes/SynologyGDriveUpload" --log-file="/volume1/scripts/rclone.log" -v
 
 echo "[$DATE] Finished sync." >> /volume1/scripts/rclone.log
