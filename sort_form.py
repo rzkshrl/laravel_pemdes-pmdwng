a007db7a552ee924f7528d10a1b38a9303930e76
rzkshrl <rizkysahrul0@gmail.com>
2025-09-01 13:49:48 +0700

    coba piton sendiri hehe

--- sort_form.py
+++ sort_form.py
@@ -5,35 +5,39 @@ import pandas as pd
 # Path NAS
 FORM_RESPONSES_XLSX = "/volume1/homes/adminpemdes/FormResponseGDriveSync/Respon Form Desa.xlsx"
 SRC_DIR = "/volume1/homes/adminpemdes/SynologyGDriveUpload/"
-DEST_BASE = "/volume1/homes/adminpemdes/PemdesData/Data Desa/"
+DEST_BASE = "/volume1/PemdesData/Data Desa/"
 
-df = pd.read_excel(FORM_RESPONSES_XLSX)
+df = pd.read_excel(FORM_RESPONSES_XLSX, sheet_name="Data Bersih")
 
 df.columns = df.columns.str.strip()   # hapus spasi berlebih
 
 # Debug: tampilkan kolom
 print("Kolom terbaca:", df.columns.tolist())
 
+KOLOM_KEC = "Kecamatan"
 KOLOM_DESA = "Desa"
 KOLOM_LINK = "Upload File"
-KOLOM_NAMA_FILE = "Nama File Asli"
+KOLOM_NAMA_FILE = "Nama File"
 
 for _, row in df.iterrows():
-    desa = str(row[KOLOM_DESA]).strip()
+    kecamatan = str(row[KOLOM_KEC]).strip()
+    desaspc = str(row[KOLOM_DESA]).strip()
+    desa = desaspc.replace(" ", "_")
     link = str(row[KOLOM_LINK]).strip()
     fname = str(row[KOLOM_NAMA_FILE]).strip()
 
     print(f"[INFO] Desa={desa}, File={fname}, Link={link}")
 
-    desa_folder = os.path.join(DEST_BASE, desa)
-    os.makedirs(desa_folder, exist_ok=True)
+    # Buat folder jika belum ada
+    kec_folder = os.path.join(DEST_BASE, kecamatan)
+    desa_folder = os.path.join(DEST_BASE/kec_folder, desa)
 
     src_file = os.path.join(SRC_DIR, fname)
-    dest_file = os.path.join(desa_folder, fname)
+    dest_file = os.path.join(desa_folder, fname)         
 
     if os.path.exists(src_file):
         if not os.path.exists(dest_file):
-            print(f"Memindahkan {fname} -> {desa_folder}")
+            print(f"Memindahkan {fname} -> {kec_folder}/{desa_folder}/")
             shutil.move(src_file, dest_file)
     else:
         print(f"[WARNING] File {fname} tidak ditemukan di {SRC_DIR}")
