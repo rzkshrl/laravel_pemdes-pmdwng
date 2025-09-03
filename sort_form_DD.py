cf399e251d72c26db43f16c5a8c38624e526225d
rzkshrl <rizkysahrul0@gmail.com>
2025-09-03 10:13:21 +0700

    update code

--- sort_form_DD.py
+++ sort_form_DD.py
@@ -8,17 +8,17 @@ SRC_DIR_EARMARK = "/volume1/homes/adminpemdes/SynologyGDriveUpload/EARMARK"
 SRC_DIR_NON_EARMARK = "/volume1/homes/adminpemdes/SynologyGDriveUpload/NON-EARMARK"
 DEST_BASE = "/volume1/PemdesData/Data Desa/"
 
+KOLOM_KEC = "Kecamatan"
+KOLOM_DESA = "Desa"
+KOLOM_LINK = "Link File"
+KOLOM_NAMA = "Nama File"
+
 dfEarmark = pd.read_excel(FORM_RESPONSES_XLSX, sheet_name="Data EARMARK")
 
 dfEarmark.columns = dfEarmark.columns.str.strip()   # hapus spasi berlebih
 
 # Debug: tampilkan kolom
-print("Kolom terbaca:", dfEarmark.columns.tolist())
-
-KOLOM_KEC = "Kecamatan"
-KOLOM_DESA = "Desa"
-KOLOM_LINK = "Link File"
-KOLOM_NAMA = "Nama File"
+print("Kolom EARMARK terbaca:", dfEarmark.columns.tolist())
 
 for _, row in dfEarmark.iterrows():
     kecamatan = str(row[KOLOM_KEC]).strip()
@@ -26,13 +26,12 @@ for _, row in dfEarmark.iterrows():
     link = str(row[KOLOM_LINK]).strip()
     fname = str(row[KOLOM_NAMA]).strip()
 
-    print(f"[INFO] Desa={desa}, File={fname}, Link={link}")
-
-    print(f"[INFO] Desa={desa}, File={fname}, Link={link}")
+    print(f"[INFO EARMARK] Desa={desa}, File={fname}, Link={link}")
 
     # Buat folder jika belum ada
     kec_folder = os.path.join(DEST_BASE, kecamatan)
     desa_folder = os.path.join(kec_folder, desa, "Dana Desa 2025", "EARMARK")
+    os.makedirs(desa_folder, exist_ok=True)
 
     src_file = os.path.join(SRC_DIR_EARMARK, fname)
     dest_file = os.path.join(desa_folder, fname)         
@@ -50,7 +49,7 @@ dfNonEarmark = pd.read_excel(FORM_RESPONSES_XLSX, sheet_name="Data NON-EARMARK")
 dfNonEarmark.columns = dfNonEarmark.columns.str.strip()   # hapus spasi berlebih
 
 # Debug: tampilkan kolom
-print("Kolom terbaca:", dfNonEarmark.columns.tolist())
+print("Kolom NON-EARMARK terbaca:", dfNonEarmark.columns.tolist())
 
 for _, row in dfNonEarmark.iterrows():
     kecamatan = str(row[KOLOM_KEC]).strip()
@@ -58,9 +57,10 @@ for _, row in dfNonEarmark.iterrows():
     link = str(row[KOLOM_LINK]).strip()
     fname = str(row[KOLOM_NAMA]).strip()
 
-    print(f"[INFO] Desa={desa}, File={fname}, Link={link}")
+    print(f"[INFO NON-EARMARK] Desa={desa}, File={fname}, Link={link}")
 
     # Buat folder jika belum ada
+    kec_folder = os.path.join(DEST_BASE, kecamatan)
     desa_folder = os.path.join(kec_folder, desa, "Dana Desa 2025", "NON-EARMARK")
     os.makedirs(desa_folder, exist_ok=True)
 
